a6f9116d969e76a07f38cad586b6e0c541cd526e
O-Turan <302287508@student.rocmondriaan.nl>
2025-11-28 10:41:43 +0100

    grote update
    de cards die ik bij diensten had moesten klikbaar zijn en naar een andere pagina gaan wat nu mogelijk is

--- src/Controller/DienstenController.php
+++ src/Controller/DienstenController.php
@@ -1,5 +1,6 @@
 <?php
 
+// src/Controller/DienstenController.php
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -8,11 +9,59 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class DienstenController extends AbstractController
 {
-    #[Route('/diensten', name: 'app_diensten')]
-    public function index(): Response
-    {
-        return $this->render('diensten/index.html.twig', [
-            'controller_name' => 'DienstenController',
-        ]);
-    }
+#[Route('/diensten', name: 'app_diensten')]
+public function index(): Response
+{
+return $this->render('diensten/index.html.twig', [
+'controller_name' => 'DienstenController',
+]);
+}
+
+#[Route('/diensten/hosted-telefonie', name: 'app_dienst_hosted_telefonie')]
+public function hostedTelefonie(): Response
+{
+return $this->render('diensten/hosted_telefonie.html.twig');
+}
+
+#[Route('/diensten/white-label', name: 'app_dienst_white_label')]
+public function whiteLabel(): Response
+{
+return $this->render('diensten/white_label.html.twig');
+}
+
+#[Route('/diensten/sip-trunk', name: 'app_dienst_sip_trunk')]
+public function sipTrunk(): Response
+{
+return $this->render('diensten/sip_trunk.html.twig');
+}
+
+#[Route('/diensten/nummerportering', name: 'app_dienst_nummerportering')]
+public function nummerportering(): Response
+{
+return $this->render('diensten/nummerportering.html.twig');
+}
+
+#[Route('/diensten/pbx', name: 'app_dienst_pbx')]
+public function pbx(): Response
+{
+return $this->render('diensten/pbx.html.twig');
+}
+
+#[Route('/diensten/hosted-pbx', name: 'app_dienst_hosted_pbx')]
+public function hostedPbx(): Response
+{
+return $this->render('diensten/hosted_pbx.html.twig');
+}
+
+#[Route('/diensten/kpn-business-partner', name: 'app_dienst_kpn')]
+public function kpn(): Response
+{
+return $this->render('diensten/kpn.html.twig');
+}
+
+#[Route('/diensten/mobiele-app', name: 'app_dienst_mobiele_app')]
+public function mobieleApp(): Response
+{
+return $this->render('diensten/mobiele_app.html.twig');
+}
 }
